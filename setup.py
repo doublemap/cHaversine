65aedbed30562532240b974424a8716ab41a69e9
Eric Jiang <eric@doublemap.com>
2015-05-26 14:06:13 -0400

    Add classifier for LGPL

--- setup.py
+++ setup.py
@@ -10,5 +10,6 @@ setup(
     author_email = 'eric@doublemap.com',
     url = 'https://github.com/doublemap/cHaversine',
     keywords = ['math', 'geo', 'cython'],
+    classifiers = ['License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)'],
     ext_modules = cythonize("cHaversine.pyx")
 )
