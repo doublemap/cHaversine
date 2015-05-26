06564a2cb30a8f55451edcbebce88dea09bbf13f
Eric Jiang <eric@doublemap.com>
2015-05-26 14:16:55 -0400

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from Cython.Build import cythonize
 setup(
     name = 'cHaversine',
     packages = ['cHaversine'],
-    version = '0.1.0',
+    version = '0.1.1',
     description = 'Fast haversine calculation',
     author = 'Eric Jiang',
     author_email = 'eric@doublemap.com',
