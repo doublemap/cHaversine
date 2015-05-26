c3d6f10ab40e04cf7727aba1cfdc078d9eef3b1c
Eric Jiang <eric@doublemap.com>
2015-05-26 15:22:07 -0400

    Use setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
     name = 'cHaversine',
     packages = ['cHaversine'],
-    version = '0.1.2',
+    version = '0.1.3',
     description = 'Fast haversine calculation',
     author = 'Eric Jiang',
     author_email = 'eric@doublemap.com',
-    requires = 'cython',
+    install_requires = 'cython',
     url = 'https://github.com/doublemap/cHaversine',
     keywords = ['math', 'geo', 'cython'],
     classifiers = ['License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)'],
