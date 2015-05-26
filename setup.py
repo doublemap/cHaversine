5975b92c9854e0f5c9a606187752d6bf7f792770
Eric Jiang <eric@doublemap.com>
2015-05-26 13:48:43 -0400

    Add metadata to setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,13 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
+    name = 'cHaversine',
+    packages = ['cHaversine'],
+    version = '0.0.1',
+    description = 'Fast haversine calculation',
+    author = 'Eric Jiang',
+    author_email = 'eric@doublemap.com',
+    url = 'https://github.com/doublemap/cHaversine',
+    keywords = ['math', 'geo', 'cython'],
     ext_modules = cythonize("cHaversine.pyx")
 )
